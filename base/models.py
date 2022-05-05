71fc8b123e1055c78255ec497951dbd0dff5842a
Dawit <dawithayl1994@gmail.com>
2022-05-05 15:19:57 +0300

    finished basic project structure

--- base/models.py
+++ base/models.py
@@ -1,12 +1,32 @@
 from django.db import models as m
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Topic(m.Model):
+	name = m.CharField(max_length=200)
+
+	def __str__(self):
+		return self.name[0:50]
+
 class Room(m.Model):
-    #host
-    #topic
+    host = m.ForeignKey(User, on_delete=m.SET_NULL, null=True)
+    topic = m.ForeignKey(Topic, on_delete=m.SET_NULL, null=True)
     name = m.CharField(max_length=60)
     description = m.TextField()
     #participants
     created_on = m.DateTimeField(auto_now_add=True)
     updated_on = m.DateTimeField(auto_now=True)
 
+    def __str__(self):
+    	return self.name
+
+class Message(m.Model):
+	user = m.ForeignKey(User, on_delete=m.CASCADE)
+	room = m.ForeignKey(Room, on_delete=m.SET_NULL, null=True)
+	body = m.TextField()
+	created_on = m.DateTimeField(auto_now_add=True)
+	updated_on = m.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.body[:60]
--- base/views.py
+++ base/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from .models import Room 
 
 rooms = [
     {'id':1, 'name':'Django rocks', 'spiritometer':"enlightened"},
@@ -8,13 +10,13 @@ rooms = [
 
 # Create your views here.
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request, 'home.html', context)
 
 def room(request, pk):
-    for room in rooms:
-        if room['id'] == pk:
-            context = {'room': room}
-        
+    room = get_object_or_404(Room, id=pk)
+    msgs = room.message_set.filter(room=room)
+    context = {'room':room, 'msgs':msgs}
 
     return render(request, 'room.html', context)
